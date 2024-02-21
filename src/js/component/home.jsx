f254ac302253b87e5d1fffb41b7e3fb16b79449f
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-02-21 02:53:51 +0000

    new change

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,43 +13,43 @@ const Home = () => {
 
 	return (
 		<div className=" container">
-			<div className="card">
+			<div className="main">
 				<header>
 					todos
 				</header>
-				<ul>
-					<li >
-						<input
+				<div className="card p-5">
+					<ul>
+						<li >
+							<input 
 							type="text"
-							onChange={(e) => setInputValue(e.target.value)}
-							value={inputValue}
-							onKeyDown={(e) => {
-								if (e.key === "Enter") {
-									setTodos([...todos, inputValue]);
-									setInputValue("");
-								}
-							}}
-							placeholder="What needs to done?"
-						/>
-					</li>
-					<div>
-						{todos.map((item, index) => (
-							<li key={index} className="card-footer d-flex justify-content-between">
-								{item}
-								<button type="button" className="btn-close" aria-label="Close" onClick={() => setTodos(todos.filter((item, currentIndex) => index != currentIndex))}></button>
-
-								
-							</li>
-						))}
+								onChange={(e) => setInputValue(e.target.value)}
+								value={inputValue}
+								onKeyDown={(e) => {
+									if (e.key === "Enter") {
+										setTodos([...todos, inputValue]);
+										setInputValue("");
+									}
+								}}
+								placeholder="What needs to done?"
+							/>
+						</li>
+						<div>
+							{todos.map((item, index) => (
+								<li key={index} className="output d-flex justify-content-between">
+									{item}
+									<button type="button" className="btn-close" aria-label="Close" onClick={() => setTodos(todos.filter((item, currentIndex) => index != currentIndex))}></button>
+								</li>
+							))}
+						</div>
+					</ul>
+				
+					<div className="m-2 card-footer">
+						{todos.length === 0 ? (
+							<p>No item left</p>
+						) : (
+							<p>{todos.length} items left</p>
+						)}
 					</div>
-				</ul>
-
-				<div className="m-2 card-footer">
-					{todos.length === 0 ? (
-						<p>No item left</p>
-					) : (
-						<p>{todos.length} items left</p>
-					)}
 				</div>
 			</div>
 		</div>
